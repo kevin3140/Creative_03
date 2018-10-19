981154e90fab892e21b06a1b1adbe3e303a8cbb8
DallinSharp <sharp.dallin@gmail.com>
2018-10-19 05:11:37 +0000

    done

--- todo.js
+++ todo.js
@@ -4,45 +4,28 @@
  */
 angular.module('app', [])
   .controller('mainCtrl', mainCtrl)
-  .directive('avatar', avatarDirective);
+
 
 function mainCtrl ($scope) {
 
-  $scope.users = [];
+  $scope.itemList = [];
 
-  $scope.addNew = function (user) {
-    $scope.users.push({ 
-      todo: user.todo,
+  $scope.addNew = function (item) {
+    $scope.itemList.push({ 
+      todo: item.todo,
       
-      dates:user.dates
-    }); /* [1] */
+      dates:item.dates
+    }); 
     
-    user.name = ''; /* [2] */
+    item.todo = ''; 
     
-    user.email='';
-  };
-}
-
-
-function avatarDirective () {
-  return {
-    scope: {
-      user: '=' /* [1] */
-    },
-    restrict: 'E', /* [2] */
-    replace: 'true',
-    template: (
-       '<div class="Avatar">' +
-      '<h4>   {{user.todo}} due  {{user.dates}} </h4>' +
-      '</div>'
-    ), /* [3] */
-    link: link
+    item.dates='';
   };
   
-  function link (scope) { /* [4] */
-    if (!scope.user.avatarUrl) {
-      scope.user.avatarUrl = 'https://www.drupal.org/files/issues/default-avatar.png';
-    }
-  }
+ $scope.removeElement = function(index)
+  {
+     $scope.itemList.splice(index, 1)
 
+  };
+  
 }
